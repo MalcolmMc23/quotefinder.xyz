344a00d6d64edcff65d7dd305bd0b8760b14bdea
Malcolm McDonald <malcolmemcdonald@gmail.com>
2024-10-24 14:24:07 -0700

    some stuff

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    // callbackURL: "http://localhost:3000/auth/google/callback"
+    // callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    callbackURL: "http://localhost:3000/auth/google/callback"
 }, async (req, accessToken, refreshToken, profile, done) => {
     let client;
     try {
